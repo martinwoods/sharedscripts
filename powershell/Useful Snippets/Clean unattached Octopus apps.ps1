a8e25c7e7b76caf1fe7f8bcacce3a232f9ffe0ad
Keith Douglas <keith.douglas@retailinmotion.com>
2019-12-02 13:51:53 +0000

    Fixing order of functions in Clean unattached Octopus apps.ps1

--- powershell/Useful Snippets/Clean unattached Octopus apps.ps1
+++ powershell/Useful Snippets/Clean unattached Octopus apps.ps1
@@ -1,25 +1,7 @@
 Import-Module WebAdministration
 Add-Type -AssemblyName Microsoft.VisualBasic
 
-$directories=Get-ChildItem "D:\Octopus\Apps\New\*\*\*\*" 
-
-Foreach ($dir in $directories) {
-
-    $webapp=$(Get-WebApplication | Where-Object {$_.PhysicalPath -eq $dir.FullName})
-    $website=$(Get-Website | Where-Object {$_.PhysicalPath -eq $dir.FullName})
-
-    If($webapp -ne $null ) {
-        Write-Output "Found Web App $($webapp.ApplicationPool) at $($dir.FullName)"
-    } elseif ( $website -ne $null ) {
-        Write-Output "Found Web Site $($webapp.ApplicationPool) at $($dir.FullName)"
-    } else {
-        Write-Warning "No webapp found for $($dir.FullName)"
-        Write-Warning "Moving $($dir.FullName) to Recycle Bin"
-        Remove-Item-ToRecycleBin($dir.FullName)
-    }
-}
-
-
+$directories=Get-ChildItem "E:\Octopus\Applications\*\*\*\*" 
 
 
 function Remove-Item-ToRecycleBin($Path) {
@@ -42,3 +24,22 @@ function Remove-Item-ToRecycleBin($Path) {
         }
     }
 }
+
+Foreach ($dir in $directories) {
+
+    $webapp=$(Get-WebApplication | Where-Object {$_.PhysicalPath -eq $dir.FullName})
+    $website=$(Get-Website | Where-Object {$_.PhysicalPath -eq $dir.FullName})
+
+    If($webapp -ne $null ) {
+        Write-Output "Found Web App $($webapp.ApplicationPool) at $($dir.FullName)"
+    } elseif ( $website -ne $null ) {
+        Write-Output "Found Web Site $($webapp.ApplicationPool) at $($dir.FullName)"
+    } else {
+        Write-Warning "No webapp found for $($dir.FullName)"
+        Write-Warning "Moving $($dir.FullName) to Recycle Bin"
+        Remove-Item-ToRecycleBin($dir.FullName)
+    }
+}
+
+
+
