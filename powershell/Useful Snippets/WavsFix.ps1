29844a41c67da16a72713527cead7aeeee1ca1cc
Vlad Petrescu <vlad.petrescu@retailinmotion.com>
2021-04-16 11:32:40 +0300

    Added servername print in WavsFix

--- powershell/Useful Snippets/WavsFix.ps1
+++ powershell/Useful Snippets/WavsFix.ps1
@@ -1,5 +1,6 @@
 $servers = @("rimpen-web-01","rimpen-web-02")
 foreach ($server in $servers){
+    Write-Host "Running on $server"
     Invoke-Command -ComputerName $server -ScriptBlock {
         Stop-Service -Name "Tomcat9" -Force
         Get-ChildItem -Path "C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps" -Filter "EposWS_WAVS_TEST*" | Remove-Item -Recurse -Force
